64c05e4613e24165addfda63b44a69418323ae01
Hossein Douzandeh <Douzandeh.k@email.come>
2023-01-16 10:49:21 +0330

    SetTimeout and SetInterval

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-setTimeout(
-    () => alert("Welcome to my site!"), 3000
-    // () => console.log("Welcome to my site"), 3000
+var x = 1;
+
+let doSomthing = () => console.log(x++);
+
+
+setInterval(
+    doSomthing, 1000
 )
+
+
+// setTimeout(
+//     () => alert("Welcome to my site!"), 3000
+//     () => console.log("Welcome to my site"), 3000
+// )
